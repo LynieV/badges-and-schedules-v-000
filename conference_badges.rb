41b88fbd58a6d9be889e9655d2a717ed37cc36db
Valerie Lynie McClimon <vmcclimon33@yahoo.com>
2018-03-25 16:09:06 +0000

    automatically backed up by learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -17,7 +17,7 @@ end
 def printer(attendees)
   batch_badge_creator(attendees).each do |badges|
   end
-  
+  puts batch_badge_creator(attendees)
   assign_rooms(attendees).each do |speaker|
   puts assign_rooms(attendees)
   end
