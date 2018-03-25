3332935f940ffa2b492205d9a9bc85e0c4261d30
Valerie Lynie McClimon <vmcclimon33@yahoo.com>
2018-03-25 14:44:18 +0000

    automatically backed up by learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,15 @@
 # Write your code here.
+def badge_maker(badges)
+  "Hello, my name is #{badges}."
+end
+
+def batch_badge_creator(attendees)
+  badges = ["Edsger", "Ada", "Charles", "Alan", "Grace", "Linus", "Matz"]
+  attendees.collect {|badges| badge_maker(badges)}
+end
+
+def assign_rooms(attendees)
+  counter = 1
+  "Hello, #{}! You'll be assigned to room #{counter}!"
+  counter += 1
+end
