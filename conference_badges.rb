26c7fe83c6317de9af62b86db9f9637c4883ce27
Valerie Lynie McClimon <vmcclimon33@yahoo.com>
2018-03-25 15:40:02 +0000

    automatically backed up by learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -15,7 +15,7 @@ def assign_rooms(attendees)
 end
 
 def printer(attendees)
-  attendees.map {|batch_badge_creator(attendees), assign_rooms(attendees)|
+  attendees.map |batch_badge_creator(attendees), assign_rooms(attendees)|
   puts batch_badge_creator(attendees)
-  puts assign_rooms(attendees)}
+  puts assign_rooms(attendees)
 end
